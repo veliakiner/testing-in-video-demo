b568320c9e0de5bf8fb664ac0463f8774485d07b
Veli Akiner <veli@kopernio.com>
2019-03-18 02:09:54 +0000

    wrap block in try

--- test/test_log.py
+++ test/test_log.py
@@ -16,10 +16,11 @@ def test_add_read_multiple():
     assert len(list_pokedex()) == 2
 
 
-@pytest.mark.xfail(reason="A bug with retrieving pokedex")
 def test_add_read_multiple_with_duplicate():
     add_to_pokedex("Bulbasaur", False)
     add_to_pokedex("Squirtle", False)
     add_to_pokedex("Squirtle", False)
-    assert len(list_pokedex()) == 2
-
+    try:
+        assert len(list_pokedex()) == 2
+    except AssertionError:
+        pytest.xfail("A bug with retrieving pokedex")
