9a028181a0a99aa87aa8add23cfe0f1f5916a38b
Veli Akiner <veli@kopernio.com>
2019-03-18 02:15:19 +0000

    force passing test to fail

--- test/conftest.py
+++ test/conftest.py
@@ -5,3 +5,17 @@ import pytest
 @pytest.fixture(autouse=True)
 def initialise_db():
     init_db()
+
+
+
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if call.when == "call":
+        if rep.outcome == "passed" and hasattr(rep, "wasxfail"):
+            # Test passed when it shouldn't've? Make it fail so everyone knows about it!
+            rep.outcome = "failed"
+    return rep
