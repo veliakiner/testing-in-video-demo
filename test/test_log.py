b1649d3482d66952936ff6d400bcd1721c4b3fac
Veli Akiner <veli@kopernio.com>
2019-03-19 22:22:36 +0000

    add notes

--- test/test_log.py
+++ test/test_log.py
@@ -9,7 +9,7 @@ def test_add_read():
     add_to_pokedex("Bulbasaur", False)
     assert len(list_pokedex()) == 1
 
-@pytest.mark.xfail(reason="Bug with list_pokedex")
+@pytest.mark.xfail(reason="Bug with list_pokedex") # xfail a test is going to fail
 def test_add_read_multiple():
     add_to_pokedex("Charmander", False)
     add_to_pokedex("Bulbasaur", False)
@@ -20,11 +20,12 @@ def test_add_read_multiple_with_duplicate():
     add_to_pokedex("Bulbasaur", False)
     add_to_pokedex("Squirtle", False)
     add_to_pokedex("Squirtle", False)
-    with xfail(AssertionError, "bug with list_pokedex"):
+    with xfail(AssertionError, "bug with list_pokedex"): # only raise an xfail if failure happens at expected point in code
         assert len(list_pokedex()) == 2
 
 
 @flaky.flaky(max_runs=4, min_passes=2)
 def test_present_in_dex():
+    # Make a test pass that fails sometimes
     add_to_pokedex("Bulbasaur", True)
     assert present_in_pokedex("Bulbasaur", True)
