24b7742597ab6e9444ce9c2928a05fd97c1c1eec
Veli Akiner <veli@kopernio.com>
2019-03-19 22:16:50 +0000

    re-add patches to see tests fail

--- test/test_log.py
+++ test/test_log.py
@@ -1,15 +1,15 @@
 from src.pogo_log import add_to_pokedex, list_pokedex, present_in_pokedex
 import pytest
 from test.lib import xfail
-
-
+import pytest
+import flaky
 
 def test_add_read():
     # Verify we can add to our Pokedex and read from it
     add_to_pokedex("Bulbasaur", False)
     assert len(list_pokedex()) == 1
 
-
+@pytest.mark.xfail(reason="Bug with list_pokedex")
 def test_add_read_multiple():
     add_to_pokedex("Charmander", False)
     add_to_pokedex("Bulbasaur", False)
@@ -20,10 +20,11 @@ def test_add_read_multiple_with_duplicate():
     add_to_pokedex("Bulbasaur", False)
     add_to_pokedex("Squirtle", False)
     add_to_pokedex("Squirtle", False)
-    assert len(list_pokedex()) == 2
+    with xfail(AssertionError, "bug with list_pokedex"):
+        assert len(list_pokedex()) == 2
 
 
-
+@flaky.flaky(max_runs=4, min_passes=2)
 def test_present_in_dex():
     add_to_pokedex("Bulbasaur", True)
     assert present_in_pokedex("Bulbasaur", True)
